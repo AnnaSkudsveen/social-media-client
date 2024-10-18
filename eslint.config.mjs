1f22b56d7ba30455cac82db2a3a314ba9fcf2dd2
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-10-18 15:43:01 +0200

    fix: configures ESLint to work with Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended
 ];
+
+module.exports = {
+  env: {
+    jest: true
+  },
+  plugins: ["jest"],
+  extends: ["plugin:jest/recommended"]
+};
