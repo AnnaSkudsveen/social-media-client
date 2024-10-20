b7f8340b5c1fdbcb821be5fceb059058bb8f4b8b
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-10-20 13:37:21 +0200

    test: Write test to check if user can login with valid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,29 @@
 import { cy } from "cypress";
 
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+// describe("template spec", () => {
+//   it("passes", () => {
+//     cy.visit("https://example.cypress.io");
+//   });
+// });
+
+describe("Login Form", () => {
+  it("should allow user to log in with valid credentials", () => {
+    // Visit the login page
+    cy.visit("/index.html");
+
+    cy.get('button[data-auth="login"]').first().click();
+
+    // Enter valid username and password
+    cy.get("#loginEmail").click().type("testeranna@stud.noroff.no");
+
+    cy.get("#loginPassword").click().type("valid_password");
+
+    // Submit the login form
+    cy.get('#loginForm button[type="submit"]')
+      .should("have.text", "Login")
+      .click();
+
+    // Assert that the user is logged in
+    cy.url().should("include", "view=profile"); // Assuming your dashboard URL
   });
 });
